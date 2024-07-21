2e36b792c948197330a2408558986da6e22ddc30
Name <email@example.ru>
2024-07-21 15:37:16 +0300

    Добавьте файлы проекта.

---
+++ MyTelegramBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VoiceTexterBot;
+using VoiceTexterBot.Controllers;
+
+namespace UtilityBot
+{
+    static class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Starting Service");
+            await host.RunAsync();
+            Console.WriteLine("Service stopped");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7364659404:AAHcUN51Ug9QaoYjjISkIBtqFgF4QcKBnsA"));
+            services.AddHostedService<Bot>();
+        }
+    }
+}
---
+++ MyTelegramBot/Сontrollers/VoiceMessageController.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+
+public class VoiceMessageController
+{
+    private readonly ITelegramBotClient _telegramClient;
+
+    public VoiceMessageController(ITelegramBotClient telegramBotClient)
+    {
+        _telegramClient = telegramBotClient;
+    }
+
+    public async Task Handle(Message message, CancellationToken ct)
+    {
+        Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+        await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено голосовое сообщение", cancellationToken: ct);
+    }
+}
